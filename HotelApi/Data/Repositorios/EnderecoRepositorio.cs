8ced7a04c1f58d7adb5eaca5a943f7ee9ab26f7c
Maiara Garcia <maiara.garcia@bevicred.com.br>
2020-12-26 16:17:10 -0300

    comitando os novos repositorios

---
+++ HotelApi/Data/Repositorios/EnderecoRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using HotelApi.Dominio.Entidades;
+using HotelApi.Dominio.Repositorio;
+
+namespace HotelApi.Data.Repositorios
+{
+    public class EnderecoRepositorio : Repositorio<Endereco>, IEnderecoRepositorio
+    {
+        public EnderecoRepositorio(ApplicationContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ HotelApi/Data/Repositorios/PessoaRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using HotelApi.Dominio.Entidades;
+using HotelApi.Dominio.Repositorio;
+
+namespace HotelApi.Data.Repositorios
+{
+    public class PessoaRepositorio : Repositorio<Pessoa>, IPessoaRepositorio
+    {
+        public PessoaRepositorio(ApplicationContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
